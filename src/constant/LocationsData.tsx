d08f7a2977774499123bc06366896f015d601bcd
Cenk Merk <cenkmerk@gmail.com>
2023-08-24 09:01:13 +0300

    Created LocationsData

---
+++ src/constant/LocationsData.tsx
@@ -0,0 +1,92 @@
+export const LocationsData = [
+  {
+    id: 1,
+    city: "Eskişehir",
+    location: [
+      {
+        locationName: "Tepebaşı",
+        address: "İsmet İnönü-1 Bulvar No:67/A",
+        pickup: true,
+        delivery: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    city: "İstanbul",
+    location: [
+      {
+        locationName: "Avcılar Çarşı",
+        address: "Merkez, Marmara Cd. No:25/2",
+        pickup: true,
+        delivery: true,
+      },
+      {
+        locationName: "Bağcılar",
+        address: "Sancaktepe Mah. Çarşı Cd. No:25/A",
+        pickup: true,
+        delivery: false,
+      },
+      {
+        locationName: "Bakırköy",
+        address: "Zeytinlik Mah. Fişekhane Cd. No:18/A",
+        pickup: false,
+        delivery: true,
+      },
+      {
+        locationName: "Esenler",
+        address: "Mimarsinan Mah., Davutpaşa Cd. No:15",
+        pickup: true,
+        delivery: true,
+      },
+      {
+        locationName: "Pendik",
+        address: "Batı Mah., Erol Kaya Cd. No:135/A",
+        pickup: false,
+        delivery: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    city: "İzmir",
+    location: [
+      {
+        locationName: "Optimum",
+        address: "Akçay Cad. Optimum AVM",
+        pickup: false,
+        delivery: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    city: "Kocaeli",
+    location: [
+      {
+        locationName: "Gebze",
+        address: "Hacı Hilal Mah. İsmet Paşa Cad. No:35/A",
+        pickup: true,
+        delivery: true,
+      },
+      {
+        locationName: "İzmit",
+        address: "Karabaş Mah. Hürriyet Cad. No:175/1",
+        pickup: true,
+        delivery: true,
+      },
+    ],
+  },
+  {
+    id: 5,
+    city: "Tekirdağ",
+    location: [
+      {
+        locationName: "Çorlu Heykel",
+        address: "Salih Omurtak Cad. No:170/B",
+        pickup: true,
+        delivery: true,
+      },
+    ],
+  },
+];
