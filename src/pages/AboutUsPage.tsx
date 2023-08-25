6f4596a3567bfc3fead2bdaf56a78d10787f6894
Cenk Merk <cenkmerk@gmail.com>
2023-08-25 17:35:15 +0300

    Completed AboutUsPage

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Stack, Typography, Container } from "@mui/material";
+import React from "react";
+import { AboutUsTexts } from "../constant/AboutUsTexts";
 
 const AboutUsPage = () => {
   return (
-    <div>AboutUsPage</div>
-  )
-}
+    <>
+      <Stack position="relative">
+        <img src="hakkimizdaimg.png" alt="" />
+        <Typography
+          variant="h2"
+          color="white"
+          position="absolute"
+          bottom="-15px"
+          right="0px"
+        >
+          ABOUT US
+        </Typography>
+      </Stack>
+      <Stack gap="10px">
+        <Typography variant="h3">Burger Yiyelim</Typography>
+        <br />
+        <Typography variant="body2">{AboutUsTexts[0]}</Typography>
+        <Typography variant="body2">{AboutUsTexts[1]}</Typography>
+        <Typography variant="body2">{AboutUsTexts[2]}</Typography>
+        <Typography variant="body2">{AboutUsTexts[3]}</Typography>
+        <Typography variant="body2">{AboutUsTexts[4]}</Typography>
+        <Typography variant="h6">NEDEN UYGUN FİYAT?</Typography>
+        <Typography variant="body2">{AboutUsTexts[5]}</Typography>
+        <Typography variant="h6">KÖFTELERİNİZ HELAL Mİ?</Typography>
+        <Typography variant="body2">{AboutUsTexts[6]}</Typography>
+      </Stack>
+    </>
+  );
+};
 
-export default AboutUsPage
+export default AboutUsPage;
