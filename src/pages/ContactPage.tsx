c657e7487becef2aef096631b160b835807ca4d3
Cenk Merk <cenkmerk@gmail.com>
2023-08-25 18:37:34 +0300

    Completed FranchisingPage

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -48,13 +48,14 @@ const FormInput = styled(TextField)({
 const SendButton = styled(Button)({
   backgroundColor: "#db1e38",
   color: "white",
-  border:"none",
-  padding:"10px 0px",
-  fontWeight:"700",
-  fontSize:"18px",
-  ":hover":{
+  border: "none",
+  padding: "10px 0px",
+  fontWeight: "700",
+  fontSize: "18px",
+  marginTop: "20px",
+  ":hover": {
     backgroundColor: "#db1e38",
-    opacity:"0.9"
-  }
+    opacity: "0.9",
+  },
 });
 export default ContactPage;
--- src/pages/FranchisingPage.tsx
+++ src/pages/FranchisingPage.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { Button, Stack, TextField, Typography, styled } from "@mui/material";
 
 const FranchisingPage = () => {
   return (
-    <div>FranchisingPage</div>
-  )
-}
-
-export default FranchisingPage
+    <>
+      <Stack position="relative">
+        <img src="franchising.jpg" alt="" />
+        <Typography
+          variant="h2"
+          color="white"
+          position="absolute"
+          bottom="-15px"
+          right="0px"
+        >
+          FRANCHISING
+        </Typography>
+      </Stack>
+      <Stack gap="20px" marginTop="20px">
+        <Typography variant="h5" gutterBottom color="#db1e38">
+          Franchising - Pre-Application Form
+        </Typography>
+        <FormInput label="Name Surname" variant="outlined" />
+        <Stack
+          sx={{
+            flexDirection: { sx: "column", md: "row" },
+            justifyContent: { md: "space-between" },
+            gap: "20px",
+          }}
+        >
+          <FormInput label="E-Mail Address" variant="outlined" fullWidth />
+          <FormInput label="Phone Number" variant="outlined" fullWidth />
+        </Stack>
+        <SendButton>Send</SendButton>
+      </Stack>
+    </>
+  );
+};
+const FormInput = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "#db1e38",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#B2BAC2",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#E0E3E7",
+    },
+    "&:hover fieldset": {
+      borderColor: "#B2BAC2",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#6F7E8C",
+    },
+  },
+});
+const SendButton = styled(Button)({
+  backgroundColor: "#db1e38",
+  color: "white",
+  border: "none",
+  padding: "10px 0px",
+  fontWeight: "700",
+  fontSize: "18px",
+  marginTop:"20px",
+  ":hover": {
+    backgroundColor: "#db1e38",
+    opacity: "0.9",
+  },
+});
+export default FranchisingPage;
